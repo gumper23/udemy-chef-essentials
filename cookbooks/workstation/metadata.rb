e0196cb5ca3153c12cd84b14e374843803b84b8d
Russ Smith <gumper@gumper.com>
2021-07-05 02:40:56 -0700

    Removed percona dependency

--- cookbooks/workstation/metadata.rb
+++ cookbooks/workstation/metadata.rb
@@ -6,8 +6,6 @@ description 'Installs/Configures workstation'
 version '0.1.0'
 chef_version '>= 15.0'
 
-depends 'percona'
-
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
